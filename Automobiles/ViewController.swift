75d68e8e6539b0ef971f7afc0ee292f8ce66bc94
Valentin Sergeevich Shapovalov <apovalov@gmail.com>
2019-12-26 22:49:56 +0300

    Initial Commit

---
+++ Automobiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Automobiles
+//
+//  Created by Macbook on 26.12.2019.
+//  Copyright © 2019 Valentin Shapovalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
